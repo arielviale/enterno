f922fd0a1a83d77bcb7fdc6272cb11c0786907b5
Ariel Martinez <aminstalaciones@yahoo.com.ar>
2023-08-09 20:05:00 -0300

    urls blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts     import render
+from django.views.generic import View
 
 # Create your views here.
+class BlogListView(View):
+    def get(self, request, *args, **Kwargs):
+        context={
+            
+        }
+        return render(request, 'blog_list.html', context)
--- core/urls.py
+++ core/urls.py
@@ -21,5 +21,5 @@ from .views import HomeView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name="home"),
-    path('blog/',include('blog'))
+    path('blog/',include('blog.urls', namespace='blog'))
 ]
